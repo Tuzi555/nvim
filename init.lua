99b79cdf0b0f041e876d01847b54bbca3e7de10c
tuzi555 <jakub.tuzar@gmail.com>
2024-01-05 15:06:49 +0100

    initial commit

---
+++ init.lua
@@ -0,0 +1,42 @@
+require("tuzi555")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system(
+        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+         lazypath})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
+
+local augroup = vim.api.nvim_create_augroup
+local Tuzi555Group = augroup('tuzi555', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40
+        })
+    end
+})
+
+autocmd({"BufWritePre"}, {
+    group = Tuzi555Group,
+    pattern = "*",
+    command = [[%s/\s\+$//e]]
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
---
+++ lua/tuzi555/remap.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<C-j>", vim.cmd.cnext)
+vim.keymap.set("n", "<C-k>", vim.cmd.cprev)
